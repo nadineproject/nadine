62839c32865e4db55aaf1916531bf610405caa5e
Jacob Sayles <jacob@officenomads.com>
2011-11-23 11:02:10 -0800

    New user page

--- tablet/views.py
+++ tablet/views.py
@@ -14,7 +14,12 @@ from staff.models import Member
 
 @staff_member_required
 def index(request):
-	return render_to_response('tablet/index.html',{  'members':Member.objects.active_members().order_by('user__first_name') }, context_instance=RequestContext(request))
+	return render_to_response('tablet/index.html', {'members':Member.objects.active_members().order_by('user__first_name')}, context_instance=RequestContext(request))
 
+@staff_member_required
+def user(request, username):
+	user = get_object_or_404(User, username=username)
+	member = get_object_or_404(Member, user=user)
+	return render_to_response('tablet/user.html',{'user':user, 'member':member}, context_instance=RequestContext(request))
 
 # Copyright 2011 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
