b377875c90d1edeaf32db3496dc5bd834682dc3d
Jacob Sayles <jacob@officenomads.com>
2011-11-22 17:08:34 -0800

    Now using jqmobile to make a nice member list

--- tablet/views.py
+++ tablet/views.py
@@ -1,32 +1,20 @@
-import calendar
 import traceback
-from datetime import datetime, timedelta, date
+from datetime import date, datetime, timedelta
 
-from django.db.models import Q
-from django.contrib import auth
 from django.conf import settings
-from django.core.cache import cache
-from django.core.mail import send_mail
-from django.utils import feedgenerator
-from django.utils.html import strip_tags
 from django.template import RequestContext
-from django.template import Context, loader
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.contrib.sites.models import Site
-from django.contrib.comments.models import Comment
-from django.template.loader import render_to_string
-from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.tokens import default_token_generator
-import django.contrib.contenttypes.models as content_type_models
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
-from django.http import HttpResponse, Http404, HttpResponseServerError, HttpResponseRedirect, HttpResponsePermanentRedirect
+
+from staff.models import Member
 
 @staff_member_required
 def index(request):
-   return render_to_response('tablet/index.html', {}, context_instance=RequestContext(request))
+	return render_to_response('tablet/index.html',{  'members':Member.objects.active_members().order_by('user__first_name') }, context_instance=RequestContext(request))
 
 
 # Copyright 2011 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
