b326830f985d9e14078bc9308ba47b4530b2547e
Jacob Sayles <jsayles@gmail.com>
2012-01-27 14:30:49 -0800

    cleaned up help text UI

--- members/views.py
+++ members/views.py
@@ -12,14 +12,16 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 from staff.models import Member
 from interlink.forms import MailingListSubscriptionForm
+from models import HelpText
 
 @login_required
 def index(request):
-	return render_to_response('members/index.html',{  'members':Member.objects.active_members().order_by('user__first_name') }, context_instance=RequestContext(request))
+	members = Member.objects.active_members().order_by('user__first_name') 
+	return render_to_response('members/index.html',{ 'members':members }, context_instance=RequestContext(request))
 
 @login_required
 def profile_redirect(request):
-    return HttpResponseRedirect(reverse('members.views.user', kwargs={'username':request.user.username}))
+	return HttpResponseRedirect(reverse('members.views.user', kwargs={'username':request.user.username}))
 
 @login_required
 def user(request, username):
@@ -37,5 +39,13 @@ def mail(request, username):
 			sub_form.save(user)
 			return HttpResponseRedirect(reverse('members.views.mail', kwargs={'username':user.username}))
 	return render_to_response('members/mail.html',{'user':user, 'mailing_list_subscription_form':MailingListSubscriptionForm()}, context_instance=RequestContext(request))
+
+def help_all(request):
+	help_topics = HelpText.objects.all()
+	return render_to_response('members/help.html',{'help_topics':help_topics}, context_instance=RequestContext(request))
+
+def help_topic(request, id):
+	topic = get_object_or_404(HelpText, id=id)
+	return render_to_response('members/help_topic.html',{'topic':topic}, context_instance=RequestContext(request))
 
 # Copyright 2010 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
