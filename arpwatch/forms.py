b98f1e7ca96158008dd3c8f9de2fdd5ac76577c3
Jacob Sayles <jsayles@gmail.com>
2012-02-22 16:38:25 -0800

    Now with file upload

---
+++ arpwatch/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class UploadFileForm(forms.Form):
+    #title = forms.CharField(max_length=50)
+    file  = forms.FileField()
--- arpwatch/views.py
+++ arpwatch/views.py
@@ -8,9 +8,24 @@ from django.template import Context, loader
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseServerError, HttpResponseRedirect, HttpResponsePermanentRedirect
 
+from forms import *
 from staff.models import Member
 
 def index(request):
-   return render_to_response('arpwatch/index.html', {}, context_instance=RequestContext(request))
+	if request.method == 'POST':
+		form = UploadFileForm(request.POST, request.FILES)
+		print("POST")
+		if form.is_valid():
+			print("VALID")
+			handle_uploaded_file(request.FILES['file'])
+			#return HttpResponseRedirect('/success/url/')
+	else:
+		 form = UploadFileForm()
+	
+	return render_to_response('arpwatch/index.html', {'form': form}, context_instance=RequestContext(request))
+
+def handle_uploaded_file(f):
+	for chunk in f.chunks():
+		print(chunk)
 
 # Copyright 2011 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
