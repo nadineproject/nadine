b6da2c17f997a9277973248e773a5b7fe5da3493
Jacob Sayles <jacob@officenomads.com>
2012-05-14 17:20:04 -0700

    New confirmation when you unsubscribe a member.

--- interlink/views.py
+++ interlink/views.py
@@ -25,6 +25,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse, Http404, HttpResponseServerError, HttpResponseRedirect, HttpResponsePermanentRedirect
 
 from models import MailingList, IncomingMail
+from staff.models import Member
 
 @staff_member_required
 def index(request):
@@ -39,25 +40,29 @@ def list_messages(request, list_id):
 @staff_member_required
 def list_subscribers(request, list_id):
    mailing_list = get_object_or_404(MailingList, pk=list_id)
-   return render_to_response('interlink/subscribers.html', {'mailing_list':mailing_list}, context_instance=RequestContext(request))
+   not_subscribed = Member.objects.active_members().exclude(user__in=mailing_list.subscribers.all)
+   return render_to_response('interlink/subscribers.html', {'mailing_list':mailing_list, 'not_subscribed':not_subscribed}, context_instance=RequestContext(request))
 
 @login_required
 def unsubscribe(request, list_id, username):
    mailing_list = get_object_or_404(MailingList, pk=list_id)
    user = get_object_or_404(User, username=username)
-   mailing_list.subscribers.remove(user)
-   return HttpResponseRedirect(reverse('interlink.views.list_subscribers', args=[list_id]))
+   if request.method == 'POST':
+      if request.POST.get('confirm', 'No') == "Yes":
+         mailing_list.subscribers.remove(user)
+      return HttpResponseRedirect(reverse('interlink.views.list_subscribers', args=[list_id]))
+   return render_to_response('interlink/unsubscribe.html', {'member':user.get_profile(), 'mailing_list':mailing_list}, context_instance=RequestContext(request))
 
-@login_required
+@staff_member_required
 def moderator_list(request):
    return render_to_response('interlink/moderator_list.html', {}, context_instance=RequestContext(request))
    
-@login_required
+@staff_member_required
 def moderator_inspect(request, id):
    incoming_mail = get_object_or_404(IncomingMail, pk=id)
    return render_to_response('interlink/moderator_inspect.html', {'incoming_mail':incoming_mail}, context_instance=RequestContext(request))
 
-@login_required
+@staff_member_required
 def moderator_approve(request, id):
    incoming_mail = get_object_or_404(IncomingMail, pk=id)
    if not request.user in incoming_mail.mailing_list.moderators.all():
@@ -71,7 +76,7 @@ def moderator_approve(request, id):
    incoming_mail.create_outgoing()
    return HttpResponseRedirect(reverse('interlink.views.moderator_list'))
 
-@login_required
+@staff_member_required
 def moderator_reject(request, id):
    incoming_mail = get_object_or_404(IncomingMail, pk=id)
    if not request.user in incoming_mail.mailing_list.moderators.all():
