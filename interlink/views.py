721eef2a51f1e5c78562cc4aedc9317658e5dac8
Paul Watts <paulcwatts@gmail.com>
2012-06-16 11:28:09 -0700

    Clear some warnings

--- interlink/views.py
+++ interlink/views.py
@@ -1,28 +1,10 @@
-import calendar
-import traceback
-from datetime import datetime, timedelta, date
-
-from django.db.models import Q
-from django.contrib import auth
-from django.conf import settings
-from django.core.cache import cache
-from django.core.mail import send_mail
-from django.utils import feedgenerator
-from django.utils.html import strip_tags
 from django.template import RequestContext
-from django.template import Context, loader
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.contrib.sites.models import Site
-from django.contrib.comments.models import Comment
-from django.template.loader import render_to_string
-from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.tokens import default_token_generator
-import django.contrib.contenttypes.models as content_type_models
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
-from django.http import HttpResponse, Http404, HttpResponseServerError, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponseRedirect
 
 from models import MailingList, IncomingMail
 from staff.models import Member
@@ -67,7 +49,7 @@ def unsubscribe(request, list_id, username):
 @staff_member_required
 def moderator_list(request):
    return render_to_response('interlink/moderator_list.html', {}, context_instance=RequestContext(request))
-   
+
 @staff_member_required
 def moderator_inspect(request, id):
    incoming_mail = get_object_or_404(IncomingMail, pk=id)
@@ -80,7 +62,7 @@ def moderator_approve(request, id):
       print request.user.get_full_name(), 'tried to moderate an email for %s' % incoming_mail.mailing_list.name
       return HttpResponseRedirect(reverse('interlink.views.moderator_list'))
 
-   if incoming_mail.state != 'moderate': 
+   if incoming_mail.state != 'moderate':
       print 'Tried to moderate an email which needs no moderation:', incoming_mail, incoming_mail.state
       return HttpResponseRedirect(reverse('interlink.views.moderator_list'))
    print 'accepting'
@@ -94,7 +76,7 @@ def moderator_reject(request, id):
       print request.user.get_full_name(), 'tried to moderate an email for %s' % incoming_mail.mailing_list.name
       return HttpResponseRedirect(reverse('interlink.views.moderator_list'))
 
-   if incoming_mail.state != 'moderate': 
+   if incoming_mail.state != 'moderate':
       print 'Tried to moderate an email which needs no moderation.'
       return HttpResponseRedirect(reverse('interlink.views.moderator_list'))
 
