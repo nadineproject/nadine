cf4031f333efa9614eb95cc5259bed57305e110e
Jacob Sayles <jacob@officenomads.com>
2012-12-05 12:18:41 -0800

    New guest of functionality now working right

--- tablet/static/custom-scripting.js
+++ tablet/static/custom-scripting.js
@@ -3,3 +3,17 @@ $(document).bind("mobileinit", function(){
   $.mobile.loader.prototype.options.textVisible = true;
   $.mobile.loader.prototype.options.theme = "a";
 });
+
+function showGuestForm() {
+	document.getElementById('guestform-show').style.display = 'none';
+	document.getElementById('guestform-hide').style.display = 'inline';
+	document.getElementById('guestform').style.display = 'block';
+	document.getElementById('signin').style.display = 'none';
+}
+
+function hideGuestForm() {
+	document.getElementById('guestform-show').style.display = 'inline';
+	document.getElementById('guestform-hide').style.display = 'none';
+	document.getElementById('guestform').style.display = 'none';
+	document.getElementById('signin').style.display = 'block';
+}
