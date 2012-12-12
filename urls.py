f3a284a8dc50eff8c25efa567dce01b68fe9a96a
Jacob Sayles <jacob@officenomads.com>
2012-12-11 16:46:34 -0800

    Updated the login and password reset pages to a consistant look and feel

--- urls.py
+++ urls.py
@@ -23,10 +23,10 @@ urlpatterns = patterns('',
 
    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
 
-   (r'^reset/$', 'front.views.password_reset', {'email_template_name':'front/email/password_reset_email.html'}),
-   (r'^reset/done/$', 'django.contrib.auth.views.password_reset_done'),
-   (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
-   (r'^reset/complete/$', 'django.contrib.auth.views.password_reset_complete'),
+   (r'^reset/$', 'front.views.password_reset', {'template_name': 'front/password_reset_form.html', 'email_template_name':'front/password_reset_email.html'}),
+   (r'^reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'front/password_reset_done.html'}),
+   (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'front/password_reset_confirm.html'}),
+   (r'^reset/complete/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'front/password_reset_complete.html'}),
 
    (r'^api/', include(API.urls)),
 
