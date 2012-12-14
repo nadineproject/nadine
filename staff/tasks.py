82db7955379abd58a12b8f768d689781e5f19610
Jacob Sayles <jacob@officenomads.com>
2012-12-13 16:05:00 -0800

    test task for fun and science

--- staff/tasks.py
+++ staff/tasks.py
@@ -23,5 +23,9 @@ def unsubscribe_recent_dropouts_task():
 	from models import Member
 	Member.objects.unsubscribe_recent_dropouts()
 
+@task(ignore_result=False)
+def test_tasks():
+	from django.core.mail import send_mail
+	send_mail("email test", "this is your message on drugs", settings.EMAIL_ADDRESS, ["jsayles@gmail.com",], fail_silently=False)
 
 # Copyright 2012 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
