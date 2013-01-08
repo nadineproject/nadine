2d87df7f64c381569693a3700395862faf408c9f
Jacob Sayles <jacob@officenomads.com>
2013-01-08 13:52:54 -0800

    Moved email template for password reset

--- urls.py
+++ urls.py
@@ -23,7 +23,7 @@ urlpatterns = patterns('',
 
    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
 
-   (r'^reset/$', 'views.password_reset', {'template_name': 'password_reset_form.html', 'email_template_name':'password_reset_email.html'}),
+   (r'^reset/$', 'views.password_reset', {'template_name': 'password_reset_form.html', 'email_template_name':'email/password_reset_email.txt'}),
    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'password_reset_done.html'}),
    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'password_reset_confirm.html'}),
    (r'^reset/complete/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'password_reset_complete.html'}),
