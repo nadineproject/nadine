befc35720cfc6dffbefea0a77dd00b3c85d86510
Jacob Sayles <jacob@officenomads.com>
2013-01-15 16:56:38 -0800

    added celery path

--- scripts/checkCelery.sh
+++ scripts/checkCelery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-celery inspect active -q > /dev/null 2> /dev/null
+/usr/local/bin/celery inspect active > /dev/null 2> /dev/null
 if [ $? -ne 0 ] ; then
-        echo "Celery Not Running!" | mail jacob@officenomads.com -s "Celery Error"
+        echo "Celery Not Running!" | mail jacob@officenomads.com -s "Celery Error";
 fi
