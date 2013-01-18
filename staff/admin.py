91ea31731032491e09926b8a71f2b7c69ee48912
Jacob Sayles <jacob@officenomads.com>
2013-01-18 12:07:22 -0800

    Now logging all outgoing emails

--- staff/admin.py
+++ staff/admin.py
@@ -48,5 +48,9 @@ class MembershipAdmin(StyledAdmin):
    search_fields = ('member__user__first_name', 'member__user__last_name')
 admin.site.register(Membership, MembershipAdmin)
 
+class SentEmailLogAdmin(StyledAdmin):
+	list_display=('created', 'recipient', 'subject', 'success')
+admin.site.register(SentEmailLog, SentEmailLogAdmin)
+	
 # Copyright 2010 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
--- staff/email.py
+++ staff/email.py
@@ -2,18 +2,14 @@ from datetime import datetime, time, date, timedelta
 from django.contrib.sites.models import Site
 from django.template.loader import render_to_string
 from django.core.mail import send_mail
-from models import Member, DailyLog
+from models import Member, DailyLog, SentEmailLog
 import settings, mailchimp
 
 def send_introduction(user):
 	site = Site.objects.get_current()
 	subject = "%s: Introduction to Nadine" % (site.name)
 	message = render_to_string('email/introduction.txt', {'user':user, 'site':site})
-	try:
-		send_mail(subject, message, settings.EMAIL_ADDRESS, [user.email], fail_silently=True)
-		announce_bad_email(user)
-	except:
-		announce_bad_email(user)
+	send_quietly(user.email, subject, message)
 	subscribe_to_newsletter(user.email)
 
 def subscribe_to_newsletter(email):
@@ -29,7 +25,8 @@ def send_new_membership(user):
 	membership = user.profile.last_membership()
 	subject = "%s: New %s Membership" % (site.name, membership.membership_plan.name)
 	message = render_to_string('email/new_membership.txt', {'user':user, 'membership':membership, 'site':site})
-	send_mail(subject, message, settings.EMAIL_ADDRESS, [user.email], fail_silently=False)	
+	send(user.email, subject, message)
+	announce_new_membership(user)
 
 def send_first_day_checkins():
 	now = datetime.now()
@@ -42,25 +39,49 @@ def send_first_day_checkin(user):
 	site = Site.objects.get_current()
 	subject = "%s: How was your first day?" % (site.name)
 	message = render_to_string('email/first_day.txt', {'user':user, 'site':site})
-	send_mail(subject, message, settings.EMAIL_ADDRESS, [user.email], fail_silently=False)
+	send(user.email, subject, message)
 
 def send_invalid_billing(user):
 	site = Site.objects.get_current()
 	subject = "%s: Billing Problem" % (site.name)
 	message = render_to_string('email/invalid_billing.txt', {'user':user, 'site':site})
-	send_mail(subject, message, settings.EMAIL_ADDRESS, [user.email], fail_silently=False)	
+	send(user.email, subject, message)
 
-def announce_new_member(member):
-	subject = "New User - %s" % (member)
-	message = "Team,\r\n\r\n \t%s just signed in for the first time!\r\n\r\n - Nadine" % (member)
-	send_mail(subject, message, settings.EMAIL_ADDRESS, [settings.TEAM_EMAIL_ADDRESS], fail_silently=True)
+def announce_new_user(user):
+	subject = "New User - %s" % (user.get_full_name())
+	message = "Team,\r\n\r\n \t%s just signed in for the first time!\r\n\r\n - Nadine" % (user.get_full_name())
+	send_quietly(settings.TEAM_EMAIL_ADDRESS, subject, message)
 
-def announce_need_photo(member):
-	subject = "Photo Opportunity - %s" % (member)
-	message = "Team,\r\n\r\n \t%s just signed in and we don't have a photo of them yet.\r\n\r\n - Nadine" % (member)
-	send_mail(subject, message, settings.EMAIL_ADDRESS, [settings.TEAM_EMAIL_ADDRESS], fail_silently=True)
+def announce_new_membership(user):
+	membership = user.profile.last_membership()
+	subject = "New %s: %s" % (membership.membership_plan.name, user.get_full_name())
+	message = "Team,\r\n\r\n \t%s has a new %s membership!\r\n\r\n - Nadine" % (user.get_full_name(), membership.membership_plan.name)
+	send_quietly(settings.TEAM_EMAIL_ADDRESS, subject, message)
+
+def announce_need_photo(user):
+	subject = "Photo Opportunity - %s" % (user.get_full_name())
+	message = "Team,\r\n\r\n \t%s just signed in and we don't have a photo of them yet.\r\n\r\n - Nadine" % (user.get_full_name())
+	send_quietly(settings.TEAM_EMAIL_ADDRESS, subject, message)
 
 def announce_bad_email(user):
-	subject = "Email Problem - %s" % (user)
+	subject = "Email Problem - %s" % (user.get_full_name())
 	message = "Team,\r\n\r\n \tWe had a problem sending the introduction email to '%s'.\r\n\r\n - Nadine" % (user.email)
-	send_mail(subject, message, settings.EMAIL_ADDRESS, [settings.TEAM_EMAIL_ADDRESS], fail_silently=True)
+	send_quietly(settings.TEAM_EMAIL_ADDRESS, subject, message)
+
+def send(recipient, subject, message):
+	send_email(recipient, subject, message, False)
+
+def send_quietly(recipient, subject, message):
+	send_email(recipient, subject, message, True)
+
+def send_email(recipient, subject, message, fail_silently):
+	success = False
+	try:
+		send_mail(subject, message, settings.EMAIL_ADDRESS, [recipient], fail_silently=fail_silently)
+		success = True
+	finally:
+		try:
+			log = SentEmailLog(recipient=recipient, subject=subject, success=success)
+			log.save()
+		except:
+			pass
