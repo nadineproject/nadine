d6d8b045448694f9fc81852ee089ec67d9c1d43b
Jacob Sayles <jacob@officenomads.com>
2013-03-17 12:24:55 -0700

    More robust error logging

--- staff/billing.py
+++ staff/billing.py
@@ -182,10 +182,11 @@ def run_billing(bill_time=datetime.now()):
 			#print 'Successfully created %s bills' % bill_count
 	except:
 		billing_log.note = traceback.format_exc()
-	billing_log.ended = datetime.now()
-	billing_log.successful = billing_success
-	billing_log.save()
-	#print 'Completed billing %s' % billing_success
-	if not billing_success: print billing_log.note
+	finally:
+		billing_log.ended = datetime.now()
+		billing_log.successful = billing_success
+		billing_log.save()
+		#print 'Completed billing %s' % billing_success
+		if not billing_success: print billing_log.note
 
 # Copyright 2010 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
