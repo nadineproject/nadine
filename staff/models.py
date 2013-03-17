b3433c5cbf130ba582bfcca32a0d01bd54e6a028
Jacob Sayles <jacob@officenomads.com>
2013-03-17 12:25:15 -0700

    New note field in SendEmailLog to capture errors

--- staff/models.py
+++ staff/models.py
@@ -515,6 +515,7 @@ class SentEmailLog(models.Model):
 	recipient = models.EmailField()
 	subject = models.CharField(max_length=128, blank=True, null=True)
 	success = models.NullBooleanField(blank=False, null=False, default=False)
+	note = models.TextField(blank=True, null=True)
 	def __str__(self): return '%s: %s' % (self.created, self.recipient)
 
 # Copyright 2009, 2010 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
