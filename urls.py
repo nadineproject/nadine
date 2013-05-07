c50c63437a118ce95b1bab6689cf61c163c235b1
Jacob Sayles <jacob@officenomads.com>
2013-05-07 09:22:57 -0700

    added required include

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import *
 from django.contrib import admin
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
 
 admin.autodiscover()
 
