d384f608f6e833f260c1e1360b537af78005964b
Jacob Sayles <jacob@officenomads.com>
2014-02-18 16:03:26 -0800

    removed redirect test

--- tablet/views.py
+++ tablet/views.py
@@ -139,8 +139,4 @@ def signin_user_guest(request, username, guestof):
 		
 	return HttpResponseRedirect(reverse('tablet.views.welcome', kwargs={'username':username}))
 
-def test_redirect(request):
-	username = "jacob"
-	return HttpResponseRedirect(reverse('tablet.views.welcome', kwargs={'username':username}))
-
 # Copyright 2011 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
