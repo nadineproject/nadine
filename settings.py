29758bbf5df3b047808ad03131f7361f8adf8c73
Jacob Sayles <jacob@officenomads.com>
2014-03-11 12:23:55 -0700

    better more logging

--- settings.py
+++ settings.py
@@ -160,9 +160,10 @@ from local_settings import *
 LOGGING = {
 	'version': 1,
 	'disable_existing_loggers': False,
-	 'formatters': {
-		  'verbose': {
-				'format': '%(levelname)s %(asctime)s %(message)s'
+	'formatters': {
+		'verbose': {
+			'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+			'datefmt' : "%d/%b/%Y %H:%M:%S"
 		  },
 		  'simple': {
 				'format': '%(levelname)s %(message)s'
@@ -170,7 +171,7 @@ LOGGING = {
 	 },
 	'handlers': {
 		'file': {
-			'level': 'INFO',
+			'level': 'DEBUG',
 			'class': 'logging.FileHandler',
 			'filename': LOGFILE,
 			'formatter': 'verbose',
@@ -181,20 +182,23 @@ LOGGING = {
 			'include_html': True,
 			'formatter': 'verbose',
 		}
-
-	 },
-	 'loggers': {
-		  'django': {
-				'handlers': ['file'],
-				'level': 'INFO',
-				'propagate': True,
-		  },
-		  'django.request': {
-				'handlers': ['file', 'mail_admins'],
-				'level': 'INFO',
-				'propagate': True,
-		  },
-	 },
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['file'],
+			'level': 'INFO',
+			'propagate': True,
+		},
+		'django.request': {
+			'handlers': ['file', 'mail_admins'],
+			'level': 'INFO',
+			'propagate': True,
+		},
+		'staff': {
+			'handlers': ['file'],
+			'level': 'DEBUG',
+		},
+	},
 }
 
 # Copyright 2009 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
