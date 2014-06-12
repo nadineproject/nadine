0a715b82a552e9189f46ec37e83bc7be16426b1d
Jacob Sayles <jacob@officenomads.com>
2014-06-12 11:31:31 -0700

    New email to announce incomplete tasks.

--- staff/email.py
+++ staff/email.py
@@ -146,6 +146,14 @@ def announce_need_photo(user):
 	message = "Team,\r\n\r\n \t%s just signed in and we don't have a photo of them yet. %s" % (user.get_full_name(), team_signature(user))
 	send_quietly(settings.TEAM_EMAIL_ADDRESS, subject, message)
 
+def announce_tasks_todo(user, tasks):
+	subject = "Incomplete Tasks - %s" % (user.get_full_name())
+	task_string = "Incomplete Tasks:"
+	for t in tasks:
+		task_string = "%s\r\n\t- %s" % (task_string, t)
+	message = "Team,\r\n\r\n \t%s just signed in and we need to complete a few more tasks.\r\n\r\n %s %s" % (user.get_full_name(), task_string, team_signature(user))
+	send_quietly(settings.TEAM_EMAIL_ADDRESS, subject, message)
+
 def announce_bad_email(user):
 	subject = "Email Problem - %s" % (user.get_full_name())
 	message = "Team,\r\n\r\n \tWe had a problem sending the introduction email to '%s'. %s" % (user.email, team_signature(user))
