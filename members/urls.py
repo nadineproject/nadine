763dd684ec73e42f0f7d7538334c3f9689de12c7
Jacob Sayles <jacob@officenomads.com>
2014-06-13 13:44:12 -0700

    new url to disable billing

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.shortcuts import redirect
 
+#urlpatterns = patterns('gather.views',
+#	url(r'^events/$', 'upcoming_events'),
+#)
+
 urlpatterns = patterns('members.views',
 	(r'^$', 'home'),
 	(r'^view/$', 'view_members'),
@@ -27,7 +31,11 @@ urlpatterns = patterns('members.views',
 	(r'^notifications/$', 'notifications'),
 	(r'^notifications/add/(?P<username>[^/]+)/$', 'add_notification'),
 	(r'^notifications/delete/(?P<username>[^/]+)/$', 'delete_notification'),
+	(r'^disable_billing/(?P<username>[^/]+)$', 'disable_billing'),
 	(r'^new_billing/$', 'new_billing'),
+	
+	#url(r'^events/create/(?P<location_slug>[^/]+)/$', 'my_create_event', name='gather_create_event'),
+	#(r'^events/', include(gather.urls)),	
 )
 
-# Copyright 2010 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+# Copyright 2014 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
