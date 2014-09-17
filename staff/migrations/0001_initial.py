bbdde045ee90b02e067ed935efe7bf23525d0afe
Jacob Sayles <jacob@officenomads.com>
2014-09-17 09:46:49 -0700

    changed dependency on taggit

--- staff/migrations/0001_initial.py
+++ staff/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('taggit', '0002_auto_20140911_1514'),
+        ('taggit', '0001_initial'),
     ]
 
     operations = [
