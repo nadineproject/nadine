084f14c51895164b66432a58a04b0bfcc0c41895
Jacob Sayles <jacob@officenomads.com>
2014-09-25 18:43:18 -0700

    removing registration of arp data on login for now

--- views.py
+++ views.py
@@ -31,7 +31,7 @@ def index(request):
 	if not request.user.is_authenticated(): return HttpResponseRedirect(reverse('django.contrib.auth.views.login'))
 
 	ip = request.META['REMOTE_ADDR']
-	arp.register_user_ip(request.user, ip)
+	#arp.register_user_ip(request.user, ip)
 	device = arp.device_by_ip(ip)
 	#if device and not device.user:
 	#	return HttpResponseRedirect(reverse('members.views.user_devices'))
