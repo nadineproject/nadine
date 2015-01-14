d7fbf00104b36bd6f131c8a1db98fe5367296c78
Jacob Sayles <jsayles@gmail.com>
2015-01-14 11:47:42 -0800

    cleaned up logic and flow

--- nadine/mailgun.py
+++ nadine/mailgun.py
@@ -2,6 +2,7 @@ import json
 import requests
 import datetime
 import logging
+import email.utils
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -12,45 +13,63 @@ from staff.models import Member
 logger = logging.getLogger(__name__)
 
 class MailgunException(Exception):
-    pass
+	pass
 
-def strip_email(email):
-	if '<' in email and '>' in email:
-		start = email.find('<')
-		end = email.find('>')
-		return email[start+1:end]
-	return email
+def render_templates(context, email_key):
+	text_content = None
+	html_content = None
 
-def mailgun_send(list_address, mailgun_data, files_dict=None):
+	try:
+		text_template = get_template("emails/%s.txt" % email_key)
+		if text_template:
+			text_content = text_template.render(context)
+
+		html_template = get_template("emails/%s.html" % email_key)
+		if html_template:
+			html_content = html_template.render(context)
+	except TemplateDoesNotExist:
+		pass
+
+	return (text_content, html_content)
+
+def mailgun_send(mailgun_data, files_dict=None):
 	#logger.debug("Mailgun send: %s" % mailgun_data)
 	#logger.debug("Mailgun files: %s" % files_dict)
-	
+
+	# Make sure we have what we need
+	from_name, from_address = email.utils.parseaddr(mailgun_data["from"])
+	to_name, to_address = email.utils.parseaddr(mailgun_data["to"][0])
+	subject = mailgun_data["subject"]
+	if not from_address or not to_address or not subject:
+		raise MailgunException("Mailgun data missing FROM, TO, or SUBJECT!")
+	logger.debug("from: %s, to: %s, subject: %s" % (from_address, to_address, subject))
+
+	# Clean up our bcc list
+	if "bcc" in mailgun_data:
+		bcc_list = mailgun_data["bcc"]
+		if from_address in bcc_list:
+			bcc_list.remove(from_address)
+		if to_address in bcc_list:
+			bcc_list.remove(to_address)
+		mailgun_data["bcc"] = list(set(bcc_list))
+		logger.debug("bcc: %s" % mailgun_data["bcc"])
+
+	# Attach some headers: LIST-ID, REPLY-TO, Precedence...
+	# Reply-To: list email apparently has some religious debates
+	# (http://www.gnu.org/software/mailman/mailman-admin/node11.html) 
+	# Precedence: list - helps some out of office auto responders know not to send their auto-replies. 
+	mailgun_data["h:List-Id"] = to_address
+	mailgun_data["h:Reply-To"] = to_address
+	mailgun_data["h:Precedence"] = "list"
+
+	# Make sure nothign goes out if the system is in debug mode
 	if settings.DEBUG:
 		if not hasattr(settings, 'MAILGUN_DEBUG') or settings.MAILGUN_DEBUG:
 			# We will see this message in the mailgun logs but nothing will actually be delivered
 			logger.debug("mailgun_send: setting testmode=yes")
 			mailgun_data["o:testmode"] = "yes"
 
-	# attach some headers: LIST-ID, REPLY-TO, MSG-ID, precedence...
-	# Precedence: list - helps some out of office auto responders know not to send their auto-replies. 
-	mailgun_data["h:List-Id"] = list_address
-	mailgun_data["h:Precedence"] = "list"
-		
-	# Reply-To: list email apparently has some religious debates
-	# (http://www.gnu.org/software/mailman/mailman-admin/node11.html) 
-	# but seems to be common these days 
-	mailgun_data["h:Reply-To"] = list_address
-
-	# Make sure the from and to are not in the bcc
-	from_address = strip_email(mailgun_data["from"])
-	if from_address in mailgun_data["bcc"]:
-		mailgun_data["bcc"].remove(from_address)
-	recipient = strip_email(mailgun_data["to"])
-	if recipient in mailgun_data["bcc"]:
-		mailgun_data["bcc"].remove(recipient)
-	logger.debug("from: %s, to: %s, subject: %s" % (recipient, from_address, mailgun_data["subject"]))
-	logger.debug("bcc: %s" % mailgun_data["bcc"])
-
+	# Fire in the hole!
 	resp = requests.post("https://api.mailgun.net/v2/%s/messages" % settings.MAILGUN_DOMAIN,
 		auth=("api", settings.MAILGUN_API_KEY),
 		data=mailgun_data, 
@@ -58,7 +77,6 @@ def mailgun_send(list_address, mailgun_data, files_dict=None):
 	)
 	logger.debug("Mailgun response: %s" % resp.text)
 	return HttpResponse(status=200)
-
 
 def clean_incoming(request):
 	if not request.POST:
@@ -110,7 +128,7 @@ def clean_incoming(request):
 	for attachment in request.FILES.values():
 		attachments.append(("inline", attachment))
 
-	return mailgun_data, attachments
+	return (mailgun_data, attachments)
 
 @csrf_exempt
 def staff(request):
@@ -120,17 +138,14 @@ def staff(request):
 		# mailgun requires a code 200 or it will continue to retry delivery
 		return HttpResponse(status=200)
 
-	# Build our BCC list
-	# Remove duplicates and the sender
-	bcc_list = []
-	for user in User.objects.filter(is_staff=True, is_active=True):
-		if user.email not in bcc_list:
-			bcc_list.append(user.email)
-	mailgun_data["bcc"] = bcc_list
+	# Hard code the recipient to be this address
+	mailgun_data["to"] = ["staff@%s" % settings.MAILGUN_DOMAIN, ]
+
+	# Going out to all Users that are active and staff
+	mailgun_data["bcc"] = User.objects.filter(is_staff=True, is_active=True)
 
 	# Send the message 
-	list_address = "staff@%s" % settings.MAILGUN_DOMAIN
-	return mailgun_send(list_address, mailgun_data, attachments)
+	return mailgun_send(mailgun_data, attachments)
 
 @csrf_exempt
 def team(request):
@@ -140,22 +155,18 @@ def team(request):
 		# mailgun requires a code 200 or it will continue to retry delivery
 		return HttpResponse(status=200)
 
-	# Build our BCC list
-	# Remove duplicates and the sender
-	bcc_list = []
-	for m in Member.objects.managers(include_future=True):
-		if m.user.email not in bcc_list:
-			bcc_list.append(m.user.email)
-	mailgun_data["bcc"] = bcc_list
+	# Hard code the recipient to be this address
+	mailgun_data["to"] = ["team@%s" % settings.MAILGUN_DOMAIN, ]
+
+	# Goes out to all managers
+	mailgun_data["bcc"] = Member.objects.managers(include_future=True)
 
 	# Send the message 
-	list_address = "team@%s" % settings.MAILGUN_DOMAIN
-	return mailgun_send(list_address, mailgun_data, attachments)
+	return mailgun_send(mailgun_data, attachments)
 	
 # mailgun setup example
 # match_recipient("test80085@(?P<location>.*?).mail.embassynetwork.com")
 # forward("https://embassynetwork.com/locations/\g<location>/email/test80085")
-
 @csrf_exempt
 def test80085(request):
 	try:
@@ -164,8 +175,6 @@ def test80085(request):
 		# mailgun requires a code 200 or it will continue to retry delivery
 		return HttpResponse(status=200)
 
+	mailgun_data["to"] = ["test80085@%s" % settings.MAILGUN_DOMAIN, ]
 	mailgun_data["bcc"] = ['jsayles@gmail.com']
-
-	# Send the message 
-	list_address = "test80085@%s" % settings.MAILGUN_DOMAIN
-	return mailgun_send(list_address, mailgun_data, attachments)
+	return mailgun_send(mailgun_data, attachments)
