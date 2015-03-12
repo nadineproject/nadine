e23286d8222f173ce5673be856adb4fd10b611b6
Jacob Sayles <jsayles@gmail.com>
2015-03-12 14:01:34 -0700

    added search and filter options

--- staff/admin.py
+++ staff/admin.py
@@ -94,6 +94,8 @@ admin.site.register(MemberAlert, MemberAlertAdmin)
 
 class FileUploadAdmin(StyledAdmin):
     list_display = ('uploadTS', 'user', 'name')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+    list_filter = ('document_type',)
 admin.site.register(FileUpload, FileUploadAdmin)
 
 # Copyright 2010 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
