2099b845c9c8d75359386f4ebeba4938f3e416cd
Jacob Sayles <jsayles@gmail.com>
2015-05-13 10:31:19 -0700

    added concept of persistant alerts that apply to inactive members as well.  closes 128

--- nadine/models.py
+++ nadine/models.py
@@ -828,7 +828,9 @@ class MemberAlertManager(models.Manager):
 
     def unresolved(self, key, active_only=True):
         unresolved = self.filter(key=key, resolved_ts__isnull=True, muted_ts__isnull=True)
-        if active_only:
+        # Persistant alerts apply even if a member is inactive
+        persistant = key in MemberAlert.PERSISTANT_ALERTS
+        if active_only and not persistant:
             active_users = Member.objects.active_users()
             return unresolved.filter(user__in=active_users)
         return unresolved
@@ -979,6 +981,9 @@ class MemberAlert(models.Model):
 
     # These alerts can be resolved by the system automatically
     SYSTEM_ALERTS = [MEMBER_INFO, MEMBER_AGREEMENT, UPLOAD_PHOTO, KEY_AGREEMENT, STALE_MEMBER]
+
+    # These alerts apply to even inactive members
+    PERSISTANT_ALERTS = [REMOVE_PHOTO, RETURN_DOOR_KEY, RETURN_DESK_KEY]
 
     @staticmethod
     def getDescription(key):
