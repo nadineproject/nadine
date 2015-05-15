a3d96fb7a4c6f0652d80c9b7a769aaf870c730fc
Jacob Sayles <jsayles@gmail.com>
2015-05-15 10:15:31 -0700

    moved google analytics to the settings file

---
+++ members/templatetags/settings_value.py
@@ -0,0 +1,11 @@
+# http://stackoverflow.com/questions/433162/can-i-access-constants-in-settings-py-from-templates-in-django
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+# settings value
+@register.simple_tag
+def settings_value(name):
+	return getattr(settings, name, "")
