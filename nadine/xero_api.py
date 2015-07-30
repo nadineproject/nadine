7062923cdac3d8ea4797ac883223b64b62eb8b42
Jacob Sayles <jsayles@gmail.com>
2015-07-30 13:32:14 -0700

    added invoice add example that doesn't work yet

--- nadine/xero_api.py
+++ nadine/xero_api.py
@@ -41,3 +41,38 @@ class XeroAPI:
     def get_all_contacts(self):
         xero = self.xero
         return xero.contacts.all()
+    
+    def create_invoice(self):
+        # Clearly jsut a test -- JLS
+        xero = self.xero
+        invoice = {
+                    u'Status': u'DRAFT',
+                    u'Total': u'264.00',
+                    u'CurrencyRate': u'1.000000',
+                    u'Reference': u'sdfghsfgh',
+                    u'Type': u'ACCREC',
+                    u'CurrencyCode': u'AUD',
+                    u'AmountPaid': u'0.00',
+                    u'TotalTax': u'24.00',
+                    u'Contact': {
+                        u'Name': u'Test One'
+                    },
+                    u'AmountDue': u'264.00',
+                    u'Date': datetime.date(2014, 7, 24),
+                    u'LineAmountTypes': u'Exclusive',
+                    u'LineItems': {
+                        u'LineItem': {
+                            u'AccountCode': u'200',
+                            u'TaxAmount': u'24.00',
+                            u'Description': u'fgshfsdh',
+                            u'UnitAmount': u'24.00',
+                            u'TaxType': u'OUTPUT',
+                            u'ItemCode': u'sfghfshg',
+                            u'LineAmount': u'240.00',
+                            u'Quantity': u'10.0000'
+                        }
+                    },
+                    u'SubTotal': u'240.00',
+                    u'DueDate': datetime.date(2014, 7, 24)
+                }
+        #xero.invoices.put(invoice)
