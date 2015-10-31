2570b580ebd702a89a476594a9e34fd988aa2db0
Jacob Sayles <jsayles@gmail.com>
2015-10-31 16:32:24 -0700

    new command to generate an encryption key

---
+++ hid/management/commands/generate_key.py
@@ -0,0 +1,22 @@
+import os
+import time
+import urllib
+import sys
+import requests
+
+from datetime import datetime
+
+from django.conf import settings
+from django.utils import timezone
+from django.core.exceptions import ImproperlyConfigured
+from django.core.management.base import BaseCommand, CommandError
+
+from cryptography.fernet import Fernet
+
+class Command(BaseCommand):
+    help = "Generate a new encryption key"
+    args = ""
+    requires_system_checks = False
+
+    def handle(self, *labels, **options):
+        print Fernet.generate_key()
