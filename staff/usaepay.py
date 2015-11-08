ebcb56a78e69c90a24421bb5f86c3c1a856f8e17
Jacob Sayles <jsayles@gmail.com>
2015-11-08 09:25:01 -0800

    removed pycrypto code

--- staff/usaepay.py
+++ staff/usaepay.py
@@ -1,7 +1,6 @@
 import base64, csv
 from datetime import datetime, timedelta
 from py4j.java_gateway import JavaGateway
-from Crypto.Cipher import AES
 from django.conf import settings
 
 def disableAutoBilling(username):
@@ -112,15 +111,3 @@ def get_checks_settled_by_date(year, month, day):
         if row[9] == "Returned":
             results.append({'date':row[1], 'name':row[2], 'status':row[9], 'amount':row[10], 'processed':row[13]})
     return results
-
-def authorize(username, auth_code):
-    crypto = AES.new(settings.USA_EPAY_KEY, AES.MODE_ECB)
-    decripted_username = urlsafe_b64decode(crypto.decrypt(auth_code))
-    padded_username = username[:16].zfill(16)
-    return decripted_username == padded_username
-
-
-def get_auth_code(username):
-    crypto = AES.new(settings.USA_EPAY_KEY, AES.MODE_ECB)
-    padded_username = username[:16].zfill(16)
-    return base64.urlsafe_b64encode(crypto.encrypt(padded_username))
