e7646cc3488271ae76ed5acac8a8c5ae626a3664
Jacob Sayles <jsayles@gmail.com>
2016-03-15 13:53:38 -0700

    django.core.templates is now django.templates

--- nadine/settings.py
+++ nadine/settings.py
@@ -91,7 +91,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
-                'django.core.context_processors.request',
+                'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
