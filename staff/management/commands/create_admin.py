4c75a774b76dbdbf6315039e6631bf7f03faab61
jacob <jsayles@gmail.com>
2016-06-02 12:07:27 -0700

    new command to create a simple admin user

---
+++ staff/management/commands/create_admin.py
@@ -0,0 +1,11 @@
+from django.core.management.base import NoArgsCommand, CommandError
+from django.core.files import File
+from django.contrib.auth.models import User
+
+class Command(NoArgsCommand):
+    help = "Create an admin user."
+    requires_system_checks = True
+
+    def handle_noargs(self, **options):
+        if not User.objects.filter(username="admin"):
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin')
