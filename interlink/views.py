3f356248a728df8344da3c7e2188c54c492520c4
jacob <jsayles@gmail.com>
2016-09-06 11:33:27 -0700

    named all the URLS

--- interlink/views.py
+++ interlink/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponseRedirect
 
-import interlink
 from interlink.models import MailingList, IncomingMail
 from nadine.models.core import Member
 
@@ -37,7 +36,7 @@ def subscribe(request, list_id, username):
     if request.method == 'POST':
         if request.POST.get('confirm', 'No') == "Yes":
             mailing_list.subscribers.add(user)
-        return HttpResponseRedirect(reverse(interlink.views.list_subscribers, args=[list_id]))
+        return HttpResponseRedirect(reverse('interlink_subscribers', args=[list_id]))
     return render_to_response('interlink/subscribe.html', {'member': user.get_profile(), 'mailing_list': mailing_list}, context_instance=RequestContext(request))
 
 
@@ -48,7 +47,7 @@ def unsubscribe(request, list_id, username):
     if request.method == 'POST':
         if request.POST.get('confirm', 'No') == "Yes":
             mailing_list.subscribers.remove(user)
-        return HttpResponseRedirect(reverse(interlink.views.list_subscribers, args=[list_id]))
+        return HttpResponseRedirect(reverse('interlink_subscribers', args=[list_id]))
     return render_to_response('interlink/unsubscribe.html', {'member': user.get_profile(), 'mailing_list': mailing_list}, context_instance=RequestContext(request))
 
 
@@ -68,14 +67,14 @@ def moderator_approve(request, id):
     incoming_mail = get_object_or_404(IncomingMail, pk=id)
     if not request.user in incoming_mail.mailing_list.moderators.all():
         #print(request.user.get_full_name(), 'tried to moderate an email for %s' % incoming_mail.mailing_list.name)
-        return HttpResponseRedirect(reverse(interlink.views.moderator_list))
+        return HttpResponseRedirect(reverse('interlink_moderate'))
 
     if incoming_mail.state != 'moderate':
         #print('Tried to moderate an email which needs no moderation:', incoming_mail, incoming_mail.state)
-        return HttpResponseRedirect(reverse(interlink.views.moderator_list))
+        return HttpResponseRedirect(reverse('interlink_moderate'))
     #print('accepting')
     incoming_mail.create_outgoing()
-    return HttpResponseRedirect(reverse(interlink.views.moderator_list))
+    return HttpResponseRedirect(reverse('interlink_moderate'))
 
 
 @staff_member_required
@@ -83,14 +82,14 @@ def moderator_reject(request, id):
     incoming_mail = get_object_or_404(IncomingMail, pk=id)
     if not request.user in incoming_mail.mailing_list.moderators.all():
         #print(request.user.get_full_name(), 'tried to moderate an email for %s' % incoming_mail.mailing_list.name)
-        return HttpResponseRedirect(reverse(interlink.views.moderator_list))
+        return HttpResponseRedirect(reverse('interlink_moderate'))
 
     if incoming_mail.state != 'moderate':
         #print('Tried to moderate an email which needs no moderation.')
-        return HttpResponseRedirect(reverse(interlink.views.moderator_list))
+        return HttpResponseRedirect(reverse('interlink_moderate'))
 
     incoming_mail.reject()
-    return HttpResponseRedirect(reverse(interlink.views.moderator_list))
+    return HttpResponseRedirect(reverse('interlink_moderate'))
 
 
-# Copyright 2011 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+# Copyright 2016 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
