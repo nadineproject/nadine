9be486ef3d8847f174356d2ff8e8a33657c35dfc
Alex Southwell <abkruse@gmail.com>
2016-09-30 10:11:17 -0700

    Logic correct to pass data to booking confirm page

--- members/views.py
+++ members/views.py
@@ -558,8 +558,6 @@ def create_booking(request):
     start = request.GET.get('start', str(datetime.datetime.now().hour) + ':' + str(datetime.datetime.now().minute))
     end = request.GET.get('end', str(datetime.datetime.now().hour + 2) + ':' + str(datetime.datetime.now().minute))
 
-    print start
-
     # Turn our date, start, and end strings into timestamps
     start_dt = datetime.datetime.strptime(date + " " + start, "%Y-%m-%d %H:%M")
     start_ts = timezone.make_aware(start_dt, timezone.get_current_timezone())
@@ -589,24 +587,21 @@ def create_booking(request):
         start = request.POST.get('start')
         end = request.POST.get('end')
         date = request.POST.get('date')
-        return render_to_response('members/user_confirm_booking.html', {'start':start, 'end':end, 'room':room, 'date': date}, context_instance=RequestContext(request))
+
+        return HttpResponseRedirect(reverse('member_confirm_booking', kwargs={'room': room, 'start': start, 'end': end, 'date': date}))
 
     return render_to_response('members/user_create_booking.html', {'rooms': rooms, 'hours':hours, 'start':start, 'end':end, 'date': date, 'has_av':has_av, 'floor': floor, 'has_phone': has_phone, 'ids': ids, 'reserved': reserved, 'search_block': search_block }, context_instance=RequestContext(request))
 
 @login_required
 @user_passes_test(is_active_member, login_url='member_not_active')
-def confirm_booking(request):
+def confirm_booking(request, room, start, end, date):
     user = request.user
-    room = request.GET.room
-    start = request.GET.start
-    end = request.GET.end
-    date = request.GET.date
-    page_message = None
 
     if request.method == 'POST':
         booking_form = EventForm()
         try:
             if booking_form.is_valid():
+                # booking_form.save()
                 return HttpResponseRedirect(reverse('member_confirm_booking'), {})
         except Exception as e:
             page_message = str(e)
