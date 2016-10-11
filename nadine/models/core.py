60cd105ecdedbe784a68f9d59f59a547f59a58aa
jacob <jsayles@gmail.com>
2016-10-11 11:19:22 -0700

    pulled in EmailTemplate model but it isn't in use yet

--- nadine/models/core.py
+++ nadine/models/core.py
@@ -1112,4 +1112,34 @@ def file_upload_callback(sender, **kwargs):
     MemberAlert.objects.trigger_file_upload(file_upload.user)
 post_save.connect(file_upload_callback, sender=FileUpload)
 
+
+# Not ready yet.  This was pulled in from modernomads. --JLS
+# class EmailTemplate(models.Model):
+#     ''' Template overrides for system generated emails '''
+#
+#     ADMIN_DAILY = 'admin_daily_update'
+#     GUEST_DAILY = 'guest_daily_update'
+#     INVOICE = 'invoice'
+#     RECEIPT = 'receipt'
+#     SUBSCRIPTION_RECEIPT = 'subscription_receipt'
+#     NEW_RESERVATION = 'newreservation'
+#     WELCOME = 'pre_arrival_welcome'
+#     DEPARTURE = 'departure'
+#
+#     KEYS = (
+#     (ADMIN_DAILY, 'Admin Daily Update'),
+#     (GUEST_DAILY, 'Guest Daily Update'),
+#     (INVOICE, 'Invoice'),
+#     (RECEIPT, 'Reservation Receipt'),
+#     (SUBSCRIPTION_RECEIPT, 'Subscription Receipt'),
+#     (NEW_RESERVATION, 'New Reservation'),
+#     (WELCOME, 'Pre-Arrival Welcome'),
+#     (DEPARTURE, 'Departure'),
+#     )
+#
+#     key = models.CharField(max_length=32, choices=KEYS)
+#     text_body = models.TextField(verbose_name="The text body of the email")
+#     html_body = models.TextField(blank=True, null=True, verbose_name="The html body of the email")
+
+
 # Copyright 2016 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
