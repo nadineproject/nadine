fd8526d29fa23267e9ca591cef74d7083b7e17a6
Jacob Sayles <jsayles@gmail.com>
2016-11-03 14:07:00 -0700

    added router

--- comlink/signals.py
+++ comlink/signals.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.dispatch import Signal, receiver
 
-email_received = Signal(providing_args=["instance","attachments"])
+email_received = Signal(providing_args=["instance", "attachments"])
+
+def staff_email(email, attachments):
+    pass
+
+def team_email(email, attachments):
+    pass
+
+routes = [
+    ('staff@test.officenomads.com', staff_email),
+    ('team@test.officenomads.com', team_email),
+]
 
 @receiver(email_received)
-def my_callback(sender, **kwargs):
-    print("Email Received")
-    print(kwargs)
+def router(sender, **kwargs):
+    incoming_email = kwargs['instance']
+    attachments = kwargs['attachments']
+    for r, m in routes:
+        if incoming_email.recipient == r:
+            return m(incoming_email, attachments)
+    raise RejectedMailException("No route found!")
