217a3777cde0d0809ec42f5704218eef645f178f
Jacob Sayles <jsayles@gmail.com>
2016-11-03 18:06:47 -0700

    simplified staff and team routes

--- comlink/signals.py
+++ comlink/signals.py
@@ -9,38 +9,14 @@ email_received = Signal(providing_args=["instance", "attachments"])
 
 def staff_email(email, attachments):
     mailgun_data = email.get_mailgun_data(stripped=True, footer=True)
-
-    # Pull the list this should go to and set them in the BCC
-    # Going out to all Users that are active and staff
-    bcc_list = list(User.objects.filter(is_staff=True, is_active=True).values_list('email', flat=True))
-    mailgun_data["bcc"] = bcc_list
-
-    # This goes out to staff and anyone that isn't already in the BCC list
-    to_list = ["staff@%s" % settings.MAILGUN_DOMAIN, ]
-    for to in mailgun_data["to"]:
-        if not to in bcc_list:
-            to_list.append(to)
-    mailgun_data["to"] = to_list
-
-    # Send the message
+    mailgun_data["bcc"] = list(User.objects.filter(is_staff=True, is_active=True).values_list('email', flat=True))
+    mailgun_data["to"] = mailgun_data["to"].insert(0, "staff@%s" % settings.MAILGUN_DOMAIN)
     return mailgun.mailgun_send(mailgun_data, attachments)
-
 
 def team_email(email, attachments):
     mailgun_data = email.get_mailgun_data(stripped=True, footer=True)
-
-    # Goes out to all managers
-    bcc_list = list(User.helper.managers(include_future=True).values_list('email', flat=True))
-    mailgun_data["bcc"] = bcc_list
-
-    # Hard code the recipient to be this address
-    to_list = ["team@%s" % settings.MAILGUN_DOMAIN, ]
-    for to in mailgun_data["to"]:
-        if not to in bcc_list:
-            to_list.append(to)
-    mailgun_data["to"] = to_list
-
-    # Send the message
+    mailgun_data["bcc"] = list(User.helper.managers(include_future=True).values_list('email', flat=True))
+    mailgun_data["to"] = mailgun_data["to"].insert(0, "team@%s" % settings.MAILGUN_DOMAIN)
     return mailgun_send(mailgun_data, attachments)
 
 routes = [
