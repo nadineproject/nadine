48688c2a0658c2dd4c26536bab323266d013fe5b
Jacob Sayles <jsayles@gmail.com>
2016-11-07 10:20:45 -0800

    cleaned up and commented out deprecated methods

--- nadine/utils/mailgun.py
+++ nadine/utils/mailgun.py
@@ -23,26 +23,6 @@ class MailgunException(Exception):
     pass
 
 
-def render_templates(context, email_key):
-    text_content = None
-    html_content = None
-
-    try:
-        text_template = get_template("email/%s.txt" % email_key)
-        if text_template:
-            text_content = text_template.render(context)
-
-        html_template = get_template("email/%s.html" % email_key)
-        if html_template:
-            html_content = html_template.render(context)
-    except TemplateDoesNotExist:
-        pass
-
-    #logger.debug("text_context: %s" % text_content)
-    #logger.debug("html_content: %s" % html_content)
-    return (text_content, html_content)
-
-
 def clean_mailgun_data(mailgun_data):
     # Make sure we have what we need
     from_name, from_address = email.utils.parseaddr(mailgun_data["from"])
@@ -103,11 +83,8 @@ def inject_list_headers(mailgun_data):
 
 
 def mailgun_send(mailgun_data, files=None, clean_first=True, inject_list_id=True):
-    if clean_first:
-        clean_mailgun_data(mailgun_data)
-
-    if inject_list_id:
-        inject_list_headers(mailgun_data)
+    if clean_first: clean_mailgun_data(mailgun_data)
+    if inject_list_id: inject_list_headers(mailgun_data)
 
     # Make sure nothing goes out if the system is in debug mode
     if settings.DEBUG:
@@ -125,53 +102,7 @@ def mailgun_send(mailgun_data, files=None, clean_first=True, inject_list_id=True
     return HttpResponse(status=200)
 
 
-def clean_incoming(request):
-    if not request.POST:
-        raise MailgunException("Request not a POST!")
-
-    header_txt = request.POST.get('message-headers')
-    message_headers = json.loads(header_txt)
-    message_header_keys = [item[0] for item in message_headers]
-
-    # A List-Id header will only be present if it has been added manually in
-    # this function, ie, if we have already processed this message.
-    if request.POST.get('List-Id') or 'List-Id' in message_header_keys:
-        raise MailgunException("List-Id header was found!")
-
-    # If 'Auto-Submitted' in message_headers or message_headers['Auto-Submitted'] != 'no':
-    if 'Auto-Submitted' in message_header_keys:
-        raise MailgunException("Message appears to be auto-submitted")
-
-    # Pull the variables out of the POST
-    recipient = request.POST.get('recipient')
-    from_address = request.POST.get('from')
-    subject = request.POST.get('subject')
-    body_plain = request.POST.get('body-plain')
-    body_html = request.POST.get('body-html')
-    logger.debug("Incoming from: %s, to: %s, subject: %s" % (from_address, recipient, subject))
-
-    # Add in a footer
-    text_footer = "\n\n-------------------------------------------\n*~*~*~* Sent through Nadine *~*~*~* "
-    body_plain = body_plain + text_footer
-    if body_html:
-        html_footer = "<br><br>-------------------------------------------<br>*~*~*~* Sent through Nadine *~*~*~* "
-        body_html = body_html + html_footer
-
-    # Build and return our data
-    mailgun_data = {"from": from_address,
-                    "to": [recipient, ],
-                    "subject": subject,
-                    "text": body_plain,
-                    "html": body_html,
-                    }
-
-    attachments = []
-    for attachment in request.FILES.values():
-        attachments.append(("inline", attachment))
-
-    return (mailgun_data, attachments)
-
-
+# TODO - move to email?
 def send_manage_member(user, subject=None):
     if subject == None:
         subject = "Incomplete Tasks"
@@ -190,6 +121,26 @@ def send_manage_member(user, subject=None):
     return mailgun_send(mailgun_data)
 
 
+def render_templates(context, email_key):
+    text_content = None
+    html_content = None
+
+    try:
+        text_template = get_template("email/%s.txt" % email_key)
+        if text_template:
+            text_content = text_template.render(context)
+
+        html_template = get_template("email/%s.html" % email_key)
+        if html_template:
+            html_content = html_template.render(context)
+    except TemplateDoesNotExist:
+        pass
+
+    #logger.debug("text_context: %s" % text_content)
+    #logger.debug("html_content: %s" % html_content)
+    return (text_content, html_content)
+
+
 def get_manage_member_content(user):
     if settings.DEBUG:
         site_url = ''
@@ -203,63 +154,114 @@ def get_manage_member_content(user):
     return render_templates(c, "manage_member")
 
 
-@csrf_exempt
-def staff(request):
-    try:
-        mailgun_data, attachments = clean_incoming(request)
-    except MailgunException as e:
-        # mailgun requires a code 200 or it will continue to retry delivery
-        return HttpResponse(status=200)
-
-    # Pull the list this should go to and set them in the BCC
-    # Going out to all Users that are active and staff
-    bcc_list = list(User.objects.filter(is_staff=True, is_active=True).values_list('email', flat=True))
-    mailgun_data["bcc"] = bcc_list
-
-    # This goes out to staff and anyone that isn't already in the BCC list
-    to_list = ["staff@%s" % settings.MAILGUN_DOMAIN, ]
-    for to in mailgun_data["to"]:
-        if not to in bcc_list:
-            to_list.append(to)
-    mailgun_data["to"] = to_list
-
-    # Send the message
-    return mailgun_send(mailgun_data, attachments)
-
-@csrf_exempt
-def team(request):
-    try:
-        mailgun_data, attachments = clean_incoming(request)
-    except MailgunException as e:
-        # mailgun requires a code 200 or it will continue to retry delivery
-        return HttpResponse(status=200)
-
-    # Goes out to all managers
-    bcc_list = list(User.helper.managers(include_future=True).values_list('email', flat=True))
-    mailgun_data["bcc"] = bcc_list
-
-    # Hard code the recipient to be this address
-    to_list = ["team@%s" % settings.MAILGUN_DOMAIN, ]
-    for to in mailgun_data["to"]:
-        if not to in bcc_list:
-            to_list.append(to)
-    mailgun_data["to"] = to_list
-
-    # Send the message
-    return mailgun_send(mailgun_data, attachments)
-
-@csrf_exempt
-def test(request):
-    print("Request: ")
-    print(request)
-
-    try:
-        mailgun_data, attachments = clean_incoming(request)
-    except MailgunException as e:
-        # mailgun requires a code 200 or it will continue to retry delivery
-        return HttpResponse(status=200)
-
-    mailgun_data["to"] = ["test80085@%s" % settings.MAILGUN_DOMAIN, ]
-    mailgun_data["bcc"] = ['jsayles@gmail.com']
-    #return mailgun_send(mailgun_data, attachments)
-    return HttpResponse(status=200)
+# Deprecated and moved to clean_mailgun_data
+# TODO - remove
+# def clean_incoming(request):
+#     if not request.POST:
+#         raise MailgunException("Request not a POST!")
+#
+#     header_txt = request.POST.get('message-headers')
+#     message_headers = json.loads(header_txt)
+#     message_header_keys = [item[0] for item in message_headers]
+#
+#     # A List-Id header will only be present if it has been added manually in
+#     # this function, ie, if we have already processed this message.
+#     if request.POST.get('List-Id') or 'List-Id' in message_header_keys:
+#         raise MailgunException("List-Id header was found!")
+#
+#     # If 'Auto-Submitted' in message_headers or message_headers['Auto-Submitted'] != 'no':
+#     if 'Auto-Submitted' in message_header_keys:
+#         raise MailgunException("Message appears to be auto-submitted")
+#
+#     # Pull the variables out of the POST
+#     recipient = request.POST.get('recipient')
+#     from_address = request.POST.get('from')
+#     subject = request.POST.get('subject')
+#     body_plain = request.POST.get('body-plain')
+#     body_html = request.POST.get('body-html')
+#     logger.debug("Incoming from: %s, to: %s, subject: %s" % (from_address, recipient, subject))
+#
+#     # Add in a footer
+#     text_footer = "\n\n-------------------------------------------\n*~*~*~* Sent through Nadine *~*~*~* "
+#     body_plain = body_plain + text_footer
+#     if body_html:
+#         html_footer = "<br><br>-------------------------------------------<br>*~*~*~* Sent through Nadine *~*~*~* "
+#         body_html = body_html + html_footer
+#
+#     # Build and return our data
+#     mailgun_data = {"from": from_address,
+#                     "to": [recipient, ],
+#                     "subject": subject,
+#                     "text": body_plain,
+#                     "html": body_html,
+#                     }
+#
+#     attachments = []
+#     for attachment in request.FILES.values():
+#         attachments.append(("inline", attachment))
+#
+#     return (mailgun_data, attachments)
+#
+#
+# Deprecated and moved to comlink
+# TODO - remove all 3
+# @csrf_exempt
+# def staff(request):
+#     try:
+#         mailgun_data, attachments = clean_incoming(request)
+#     except MailgunException as e:
+#         # mailgun requires a code 200 or it will continue to retry delivery
+#         return HttpResponse(status=200)
+#
+#     # Pull the list this should go to and set them in the BCC
+#     # Going out to all Users that are active and staff
+#     bcc_list = list(User.objects.filter(is_staff=True, is_active=True).values_list('email', flat=True))
+#     mailgun_data["bcc"] = bcc_list
+#
+#     # This goes out to staff and anyone that isn't already in the BCC list
+#     to_list = ["staff@%s" % settings.MAILGUN_DOMAIN, ]
+#     for to in mailgun_data["to"]:
+#         if not to in bcc_list:
+#             to_list.append(to)
+#     mailgun_data["to"] = to_list
+#
+#     # Send the message
+#     return mailgun_send(mailgun_data, attachments)
+#
+# @csrf_exempt
+# def team(request):
+#     try:
+#         mailgun_data, attachments = clean_incoming(request)
+#     except MailgunException as e:
+#         # mailgun requires a code 200 or it will continue to retry delivery
+#         return HttpResponse(status=200)
+#
+#     # Goes out to all managers
+#     bcc_list = list(User.helper.managers(include_future=True).values_list('email', flat=True))
+#     mailgun_data["bcc"] = bcc_list
+#
+#     # Hard code the recipient to be this address
+#     to_list = ["team@%s" % settings.MAILGUN_DOMAIN, ]
+#     for to in mailgun_data["to"]:
+#         if not to in bcc_list:
+#             to_list.append(to)
+#     mailgun_data["to"] = to_list
+#
+#     # Send the message
+#     return mailgun_send(mailgun_data, attachments)
+#
+# @csrf_exempt
+# def test(request):
+#     print("Request: ")
+#     print(request)
+#
+#     try:
+#         mailgun_data, attachments = clean_incoming(request)
+#     except MailgunException as e:
+#         # mailgun requires a code 200 or it will continue to retry delivery
+#         return HttpResponse(status=200)
+#
+#     mailgun_data["to"] = ["test80085@%s" % settings.MAILGUN_DOMAIN, ]
+#     mailgun_data["bcc"] = ['jsayles@gmail.com']
+#     #return mailgun_send(mailgun_data, attachments)
+#     return HttpResponse(status=200)
