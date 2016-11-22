5e1063f4f72052bb152b7ba2fa24e7f9a6bab5eb
Jacob Sayles <jsayles@gmail.com>
2016-11-22 13:16:01 -0800

    new org blurb

--- nadine/models/organization.py
+++ nadine/models/organization.py
@@ -42,8 +42,9 @@ def org_photo_path(instance, filename):
 class Organization(models.Model):
     created_ts = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name="+")
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=64, unique=True)
     lead = models.ForeignKey(User, null=True, blank=True)
+    blurb = models.CharField(max_length=112, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     photo = models.ImageField(upload_to=org_photo_path, blank=True, null=True)
     public = models.BooleanField(default=False)
