4ffa0175a591fe1d5a391f61e4d02fb288f8fff7
Jacob Sayles <jsayles@gmail.com>
2016-12-06 10:09:47 -0800

    Now with inboxes

--- comlink/views.py
+++ comlink/views.py
@@ -11,11 +11,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from comlink.forms import EmailForm
-from comlink.models import Attachment, IncomingEmail
+from comlink.models import Attachment, IncomingEmail, SimpleMailingList
 from comlink.signals import email_received
 from comlink.exceptions import RejectedMailException, DroppedMailException
 from comlink import jwzthreading
@@ -31,23 +32,45 @@ VERIFY_SIGNATURE = getattr(settings, "MAILGUN_VERIFY_INCOMING", not settings.DEB
 # User Interface Views
 ##########################################################################
 
-@login_required
+
+@staff_member_required
 def home(request):
     messages = IncomingEmail.objects.all().order_by("-received")
     threads = jwzthreading.thread(messages)
-    context = {'messages':messages, 'threads':threads}
+    inboxes = []
+    for a in SimpleMailingList.objects.all().values('address'):
+        inboxes.append({'address': a})
+    if hasattr(settings, "STAFF_EMAIL_ADDRESS"):
+        inboxes.append({'address': settings.STAFF_EMAIL_ADDRESS})
+    if hasattr(settings, "TEAM_EMAIL_ADDRESS"):
+        inboxes.append({'address': settings.TEAM_EMAIL_ADDRESS})
+    for i in inboxes:
+        c = IncomingEmail.objects.filter(recipient__contains=i['address']).count()
+        i['messages'] = c
+    context = {'messages':messages, 'inboxes':inboxes}
     return render(request, 'comlink/home.html', context)
 
-@login_required
+
+@staff_member_required
+def inbox(request, address):
+    # TODO - make sure they are able to read this email
+    messages = IncomingEmail.objects.filter(recipient__contains=address).order_by("-received")
+    context = {'messages':messages, 'address':address}
+    return render(request, 'comlink/inbox.html', context)
+
+
+@staff_member_required
 def view_mail(request, id):
     message = get_object_or_404(IncomingEmail, id=id)
     headers = message.headers
     context = {'message':message, 'headers':headers}
     return render(request, 'comlink/mail.html', context)
 
+
 ##########################################################################
 # Incoming Mail Views
 ##########################################################################
+
 
 class Incoming(View):
     email_model = IncomingEmail
