251929a3fa103d05a3ad44f84a322261624b0123
Jacob Sayles <jsayles@gmail.com>
2017-01-19 12:06:20 -0800

    removed bits of 2.0

--- nadine/models/organization.py
+++ nadine/models/organization.py
@@ -13,7 +13,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 from nadine.models.core import Website, URLType
-from nadine.models.membership import Membership, Membership2
+from nadine.models.membership import Membership
 
 from taggit.managers import TaggableManager
 
@@ -185,8 +185,9 @@ class OrganizationNote(models.Model):
         app_label = 'nadine'
 
 
-class OrganizationMembership(Membership2):
-    organization = models.ForeignKey(Organization, related_name="membership")
+# For Nadine 2.0
+# class OrganizationMembership(Membership2):
+#     organization = models.ForeignKey(Organization, related_name="membership")
 
 
 # Copyright 2017 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
