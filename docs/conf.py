06362919a89779b211b7db19e3ee8a4293f4cc9d
Alex Southwell <abkruse@gmail.com>
2017-01-26 11:00:06 -0800

    Adjust theme of conf for docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import recommonmark
+from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 
 
@@ -90,7 +92,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -159,3 +161,15 @@ texinfo_documents = [
      author, 'Nadine', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+github_doc_root = 'https://github.com/nadineproject/nadine/tree/master/docs'
+
+def setup(app):
+    app.add_config_value(
+        'recommonmark_config',
+        {
+            'url_resolver': lambda url: github_doc_root + url,
+            'enable_auto_toc_tree': True
+            'auto_toc_tree_section': 'Contents',
+        }, True)
+    app.add_transform(AutoStructify)
