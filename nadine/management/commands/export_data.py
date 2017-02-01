b4517bb7896c0766b29b24978c421e35a1061a1e
Jacob Sayles <jsayles@gmail.com>
2017-02-01 10:28:32 -0800

    Now exporting anonymous user data

--- nadine/management/commands/export_data.py
+++ nadine/management/commands/export_data.py
@@ -8,6 +8,7 @@ import json
 from nadine.models.membership import Membership
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
+from collections import OrderedDict
 
 def date_handler(x):
     if isinstance(x, datetime.date):
@@ -24,13 +25,40 @@ class Command(BaseCommand):
     def handle(self, *labels, **options):
         output = options['output']
 
-        memberships = []
-        for m in Membership.objects.all():
-            memberships.append({'user_id':m.user.id, 'start_date':m.start_date, 'end_date':m.end_date, 'monthly_rate':m.monthly_rate})
+        user_data = []
+        for user in User.objects.all().order_by('id'):
+            data = OrderedDict()
+            data['user_id'] = user.id
+            data['gender'] = user.profile.gender
+            data['howheard'] = str(user.profile.howHeard)
+            data['neighborhood'] = str(user.profile.neighborhood)
+            data['industry'] = str(user.profile.industry)
+            data['has_kids'] = user.profile.has_kids
+            data['self_employed'] = user.profile.self_employed
+            data['first_visit'] = user.profile.first_visit()
+            data['coworking_days'] = user.profile.activity().count()
+            data['billable_days'] = user.profile.paid_count()
+            data['hosted_days'] = user.profile.hosted_days().count()
+            data['has_photo'] = str(user.profile.photo) != ''
+            data['emails'] = len(user.profile.all_emails())
+            data['files'] = len(user.profile.file_uploads())
+            data['websites'] = user.profile.websites.count()
+            data['tags'] = []
+            for tag in user.profile.tags.all():
+                data['tags'].append(str(tag))
+            data['memberships'] = []
+            for m in Membership.objects.filter(user=user).order_by('start_date'):
+                m_data = OrderedDict()
+                m_data['plan'] = m.membership_plan.name
+                m_data['start_date'] = m.start_date
+                m_data['end_date'] = m.end_date
+                m_data['monthly_rate'] = m.monthly_rate
+                data['memberships'].append(m_data)
+            user_data.append(data)
 
         print("Writing JSON data to: %s" % output)
         with open(output, 'w') as outfile:
-            json.dump(memberships, outfile, default=date_handler)
+            json.dump(user_data, outfile, default=date_handler)
 
 
 
