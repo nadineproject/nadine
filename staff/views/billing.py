a34b24b5e0838fde46cab669a884cc0e745b3f9b
Alex Southwell <abkruse@gmail.com>
2017-03-27 15:12:49 -0700

    Add styling and past days count to unpaid bills in bill_view

--- staff/views/billing.py
+++ staff/views/billing.py
@@ -239,7 +239,8 @@ def toggle_billing_flag(request, username):
 @staff_member_required
 def bill_view(request, bill_id):
     bill = get_object_or_404(UserBill, id=bill_id)
-    context = {"bill": bill}
+    diff = localtime(now()).date() - bill.period_end
+    context = {"bill": bill, "diff": diff.days}
     return render(request, 'staff/billing/bill_view.html', context)
 
 
