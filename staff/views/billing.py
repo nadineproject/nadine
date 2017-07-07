3a26ca4dc570df68f20ceb6f7bb8750c2d196a48
Jacob Sayles <jsayles@gmail.com>
2017-07-07 14:11:01 -0700

    reworked outstanding bills to show open bills and removed open bill page

--- staff/views/billing.py
+++ staff/views/billing.py
@@ -61,24 +61,15 @@ def bill_list(request):
 
 @staff_member_required
 def outstanding(request):
-    outstanding = UserBill.objects.outstanding().filter(in_progress=False, closed_ts__isnull=False).order_by('due_date')
-    in_progress = UserBill.objects.outstanding().filter(in_progress=True, closed_ts__isnull=False).order_by('due_date')
+    closed_bills = UserBill.objects.outstanding().filter(closed_ts__isnull=False, in_progress=False).order_by('due_date')
+    open_bills = UserBill.objects.outstanding().filter(closed_ts__isnull=True, in_progress=False).order_by('due_date')
+    in_progress = UserBill.objects.outstanding().filter(in_progress=True).order_by('due_date')
+    bill_count = closed_bills.count() + open_bills.count() + in_progress.count()
     context = {
-        'outstanding': outstanding,
-        'open_bills': None,
-        'in_progress': in_progress,
-    }
-    return render(request, 'staff/billing/outstanding.html', context)
-
-
-@staff_member_required
-def open_bills(request):
-    open_bills = UserBill.objects.non_zero().filter(closed_ts__isnull=True, in_progress=False).order_by('due_date')
-    in_progress = UserBill.objects.outstanding().filter(closed_ts__isnull=True, in_progress=True).order_by('due_date')
-    context = {
-        'outstanding': None,
+        'closed_bills': closed_bills,
         'open_bills': open_bills,
         'in_progress': in_progress,
+        'bill_count': bill_count
     }
     return render(request, 'staff/billing/outstanding.html', context)
 
