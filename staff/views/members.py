d354c0c83527e989884f66084826d76bfe27e4b1
Alex Southwell <abkruse@gmail.com>
2017-07-17 14:14:51 -0700

    Change membership tables in member/profile

--- staff/views/members.py
+++ staff/views/members.py
@@ -412,9 +412,10 @@ def confirm_membership(request, username, package, end_target, start_target, new
     pkg = ast.literal_eval(package)
     match = None
     old_pkg = None
+    print user.membership.package_name() == None
 
     if user.membership.package_name():
-        old_pkg = MembershipPackage.objects.get(name=membership.package_name())
+        old_pkg = MembershipPackage.objects.get(name=user.membership.package_name())
 
     if len(membership.active_subscriptions(datetime.strptime(end_target, '%Y-%m-%d'))):
         ending_pkg = True
@@ -454,7 +455,7 @@ def confirm_membership(request, username, package, end_target, start_target, new
                         if user.membership.package_name() == None:
                             mailing_lists = MailingList.objects.filter(is_opt_out=True)
                             for ml in mailing_lists:
-                                ml.subscribers.add(membership.user)
+                                ml.subscriber(membership.user)
                     # Review all subscriptions to see if adding or ending
                     for sub in subs:
                         sub_id = sub['s_id']
