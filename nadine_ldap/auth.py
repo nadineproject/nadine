45352a0ee0cb4d7c20d946805ba0dc8ae7b7591b
Tom Nightingale <tom@tnightingale.com>
2017-11-07 18:06:26 -0800

    Adding nadine_ldap package
    Currently only contains custom LDAP auth backend but will be used to
    provide listeners/subscribers to Django User model crud signals to
    propagate changes to LDAP via django-ldapdb.

---
+++ nadine_ldap/auth.py
@@ -0,0 +1,35 @@
+from django_auth_ldap.backend import LDAPBackend, _LDAPUser
+from django.contrib.auth.models import User
+
+class NadineLDAPBackend(LDAPBackend):
+
+    def get_or_create_user(self, username, ldap_user):
+        """
+        Overriding get_or_create_user to perform our own Django/Nadine User
+        lookup.
+        django-auth-ldap assumes we would want to create Django/Nadine users if
+        they don't exist but we don't want to do this. Instead we throw a
+        _LDAPUser.AuthenticationFailed exception.
+        """
+        try: 
+            # Use 'nadine.backends.EmailOrUsernameModelBackend' strategy to
+            # look up a user. First by email, then by username.
+            if '@' in username:
+                user = User.helper.by_email(username)
+            else:
+                user = User.objects.get(username=username)
+
+        except User.DoesNotExist as e:
+            # TODO: Get more specific about the exception we want to catch, eg: NotFound
+            import pdb; pdb.set_trace()
+            # If we can't match against a user in the database then we bail.
+            # LDAP users are created & managed FROM Nadine; we don't want to be
+            # creating Django users for existing LDAP accounts.
+            pass
+
+        if (user):
+            return (user, user.date_joined)
+        else:
+            raise _LDAPUser.AuthenticationFailed(
+                "User exists in LDAP but doesn't exist in Nadine"
+            )
