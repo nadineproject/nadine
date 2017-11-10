55e38b50334e1c5315272e6fa6ced3c5346f7568
Tom Nightingale <tom@tnightingale.com>
2017-11-09 18:56:29 -0800

    Full LDAP user account syncing!
    Still need to get emails into LDAP but otherwise fully-functioning prototype.
    
    Added a regular database model to keep track of issues with
    creating/updating individual account's LDAP representations, it remains
    to be seen how useful it will but but certainly needs to be renamed.

--- nadine_ldap/password_validation.py
+++ nadine_ldap/password_validation.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 
 from ldapdb.backends.ldap.base import LdapDatabase
+from ldap import LDAPError
 
 import models
 
@@ -28,63 +29,52 @@ class LDAPSyncValidator(object):
     https://docs.djangoproject.com/en/1.11/topics/auth/customizing/#reusable-apps-and-auth-user-model
     """
     def validate(self, password, user=None):
-        # Called before user is saved to DB
-        # Able to bail if LDAP fails
-        # Might prematurely create LDAP account (eg: user save fails).
-        self.set_or_update_ldap_user_password(user, password)
         pass
 
     def password_changed(self, password, user=None):
-        # Always called after user is saved to DB
-        # Not called if user save fails
-        # Can't bail if LDAP fails.
-        # self.set_or_update_ldap_user_password(user, password)
-        pass
-
-    def set_or_update_ldap_user_password(self, user, password):
-        if (user is None):
-            # Nothing we can do here (except maybe log something?)
-            return
-        try:
-            # Get a group's gid; posixAccount requires user belong to a group
-            members_dn = "cn=members,ou=groups,dc=tnightingale,dc=com" # settings.NADINE_LDAP_MEMBERS_GROUP_DN
-            # TODO: we should probably get_or_create() this
-            members = models.LDAPGroup.objects.get(dn=members_dn)
-            gid = members.gid
-
-            # TODO: Move this into a helper LDAPUserManager.get_or_create()
-            try:
-                ldap_user = models.LDAPUser.objects.get(username=user.username)
-            except models.LDAPUser.DoesNotExist:
-                # Fetching last uid so we can safely set a new one.
-                # This is not ideal but apparently (based on a brief internet
-                # search) it's the only way to increment uid with LDAP.
-                lastUser = models.LDAPUser.objects.order_by('uid').first()
-                if (lastUser is not None):
-                    uid = lastUser.uid + 1
-                else:
-                    uid = 1000 # settings.NADINE_LDAP_USER_STARTING_UID
-
-                # Finally, we can create a user.
-                ldap_user = models.LDAPUser.objects.create(
-                    uid=uid,
-                    group=gid,
-                    home_directory="/home/{}".format(user.username), # settings.NADINE_LDAP_USER_HOME_DIR_TEMPLATE
-                    full_name=user.username,
-                    last_name=user.username,
-                    username=user.username
-                )
-            # End LDAPUserManager.get_or_create()
-
-            ldap_user.full_name = user.username
-            ldap_user.password = password
-            ldap_user.save()
-
-        except Exception as e:
-            raise ValidationError("There was a problem communicating with LDAP")
-            # TODO: We either, raise django.core.exceptions.ValidationError
-            #       (can't set/update user password), or we log and fail
-            #       silently (user account is created *only* in Django)
+        """
+        Always called after user is saved to DB
+        Not called if user save fails
+        Can't bail if LDAP fails.
+        """
+        set_password(user, password)
 
     def get_help_text(self):
+        """
+        Provides help text for Django Auth's password form.
+        """
         return _("Your password will also be added to LDAP.")
+
+def set_password(user, password):
+    """
+    Set the plain-text password onto the LDAP representation of the user
+    account.
+    """
+    if user is None:
+        # Nothing we can do here (except maybe log something?)
+        return
+
+    try:
+        ldap_user, created = models.LDAPPosixUser.objects.get_or_create(
+            username=user.username,
+            defaults={
+                # Adding dummy values here
+                'full_name': user.username,
+                'last_name': user.username,
+                'password': password
+            }
+        )
+        if not created:
+            ldap_user.password = password
+            ldap_user.save()
+    # TODO: ldap package produces a whole variety of exceptions, need to see if there's a better method of 
+    except LDAPError as e:
+        # Record failure in database.
+        usersLDAPAccount = models.UsersLDAPAccount.objects.update_or_create(
+            user=user,
+            defaults={
+                'has_error': True,
+                'last_error': str(e)
+            }
+        )
+        # TODO: We should log an error.
