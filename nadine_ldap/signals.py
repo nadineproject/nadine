23a789f869ef90d52131066faa3f01d84efc237f
Tom Nightingale <tom@tnightingale.com>
2018-01-09 11:50:56 -0800

    New method of syncing user account info to LDAP
    Renamed confusingly named model for logging LDAP errors.

--- nadine_ldap/signals.py
+++ nadine_ldap/signals.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save, post_save
-from models import LDAPPosixUser, UsersLDAPAccount
+from django.db.utils import Error as DBError
+
+from models import LDAPPosixUser, LDAPAccountStatus
 
 # @receiver(pre_save, sender=User)
 # def user_pre_save(**kwargs):
@@ -12,28 +14,36 @@ from models import LDAPPosixUser, UsersLDAPAccount
 @receiver(post_save, sender=User)
 def user_post_save(**kwargs):
     user = kwargs['instance']
-    LDAPPosixUser.objects.update_or_create(
-        username=user.username,
-        full_name=user.username,
-        last_name=user.username
-    )
-
-@receiver(pre_save, sender=LDAPPosixUser)
-def ldap_posix_user_pre_save(**kwargs):
     try:
-        ldap_posix_user = kwargs['instance']
-        user = User.objects.get(username=ldap_posix_user.username)
-        # LDAP write succeeded! Clear any recorded LDAP errors.
-        # TODO: Verify username change correctly propagates to ldap_dn value.
-        usersLDAPAccount = UsersLDAPAccount.objects.update_or_create(
+        LDAPPosixUser.objects.update_or_create(
+            username=user.username,
+            full_name=user.username,
+            last_name=user.username,
+            password=user.password
+        )
+    except DBError as ldap_error:
+        """
+        Something went wrong writing to LDAP. We don't want to interrupt the
+        user. Instead we record the problem in Nadine's database in a record
+        associated with the user's account.
+        """
+        LDAPAccountStatus.objects.update_or_create(
             user=user,
             defaults={
-                'ldap_dn': ldap_posix_user.dn,
-                'exists_in_ldap': True,
-                'has_error': False,
-                'last_error': ''
+                'has_error': True,
+                'last_error': str(ldap_error)
             }
         )
-    except User.DoesNotExist:
-        # TODO: Something went very wrong, log this.
-        pass
+
+@receiver(post_save, sender=LDAPPosixUser)
+def ldap_posix_user_post_save(**kwargs):
+    ldap_posix_user = kwargs['instance']
+    user = User.objects.get(username=ldap_posix_user.username)
+    LDAPAccountStatus.objects.update_or_create(
+        user=user,
+        defaults={
+            'ldap_dn': ldap_posix_user.dn,
+            'has_error': False,
+            'last_error': ''
+        }
+    )
