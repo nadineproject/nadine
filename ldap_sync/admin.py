d4cead9c1a407f0c20f107d07bc45292dbab415e
Tom Nightingale <tom@tnightingale.com>
2018-01-12 14:31:16 -0800

    Removes commented-out code in ldap_sync admin. Adds brief descriptions to the admin classes

--- ldap_sync/admin.py
+++ ldap_sync/admin.py
@@ -5,20 +5,18 @@ from nadine.admin import EmailAddressInline, UserProfileInline, EmergencyContact
 from ldap_sync.models import LDAPAccountStatus
 
 class LDAPAccountStatusInline(admin.TabularInline):
+    """
+    Defines admin widget for LDAPAccountStatus information.
+    """
     model = LDAPAccountStatus
     readonly_fields = ['synchronized', 'ldap_error_message', 'ldap_dn', ]
     can_delete = False
 
 class LDAPUserWithProfileAdmin(UserWithProfileAdmin):
+    """
+    Extends existing user profile admin with LDAPAccountStatus info.
+    """
     inlines = [EmailAddressInline, UserProfileInline, EmergencyContactInline, XeroContactInline, LDAPAccountStatusInline]
-    # list_display = ('username', 'email', 'date_joined', 'last_login')
-    # ordering = ('-date_joined', 'username')
-    # search_fields = ('username', 'first_name', 'last_name', 'emailaddress__email')
-    # readonly_fields = ('last_login', 'date_joined')
-    # fieldsets = (
-    #     (None, {'fields': ('username', 'first_name', 'last_name',
-    #         'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined', 'password')}),
-    # )
 
 admin.site.unregister(User)
 admin.site.register(User, LDAPUserWithProfileAdmin)
