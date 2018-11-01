586bc57fb0d010725348e70da3b24dfe78666849
Jacob Sayles <jsayles@gmail.com>
2018-11-01 12:19:09 -0700

    fixed bug in login.  Closes 408

--- nadine/settings/base.py
+++ nadine/settings/base.py
@@ -177,6 +177,7 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
 # URL that handles login
 LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/member/profile/'
 
 # The interface for the front desk tablet.  Values are 'WEB' or "iOS"
 #TABLET = "iOS"
