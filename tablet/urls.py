b90c56d050216bc1fc51d1f1c24dd905ba72b24b
Jacob Sayles <jsayles@gmail.com>
2019-03-05 13:28:02 -0800

    refactored to use path not url

--- tablet/urls.py
+++ tablet/urls.py
@@ -1,27 +1,25 @@
 from django.shortcuts import redirect
-from django.conf.urls import url, include
-from django.views.generic import RedirectView
+from django.urls import include, path
 
 from . import views
 
 app_name = 'tablet'
 urlpatterns = [
-    url(r'^$', lambda r: redirect('tablet:motd'), name="home"),
-
-    url(r'^members/$', views.members, name='members'),
-    url(r'^motd/$', views.motd, name='motd'),
-    url(r'^here_today/$', views.here_today, name='here_today'),
-    url(r'^visitors/$', views.visitors, name='visitors'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^welcome/(?P<username>[^/]+)$', views.welcome, name='welcome'),
-    url(r'^profile/(?P<username>[^/]+)/$', views.user_profile, name='user_profile'),
-    url(r'^post_create/(?P<username>[^/]+)/$', views.post_create, name='post_create'),
-    url(r'^(?P<username>[^/]+)/signin/$', views.signin_user, name='signin_user'),
-    url(r'^(?P<username>[^/]+)/guestof/(?P<paid_by>[^/]+)$', views.signin_user_guest, name='signin_guest'),
-    url(r'^(?P<username>[^/]+)/documents/$', views.document_list, name='document_list'),
-    url(r'^(?P<username>[^/]+)/document/(?P<doc_type>[^/]+)/$', views.document_view, name='document_view'),
-    url(r'^(?P<username>[^/]+)/signature/(?P<doc_type>[^/]+)/$', views.signature_capture, name='sig_capture'),
-    url(r'^(?P<username>[^/]+)/signature/(?P<doc_type>[^/]+)/(?P<signature_file>[^/]+)$', views.signature_render, name='sig_render'),
+    path('', lambda r: redirect('tablet:motd'), name="home"),
+    path('members/', views.members, name='members'),
+    path('motd/', views.motd, name='motd'),
+    path('here_today/', views.here_today, name='here_today'),
+    path('visitors/', views.visitors, name='visitors'),
+    path('search/', views.search, name='search'),
+    path('welcome/<username>', views.welcome, name='welcome'),
+    path('profile/<username>/', views.user_profile, name='user_profile'),
+    path('post_create/<username>/', views.post_create, name='post_create'),
+    path('<username>/signin/', views.signin_user, name='signin_user'),
+    path('<username>/guestof/<paid_by>/', views.signin_user_guest, name='signin_guest'),
+    path('<username>/documents/', views.document_list, name='document_list'),
+    path('<username>/document/<doc_type>/', views.document_view, name='document_view'),
+    path('<username>/signature/<doc_type>/', views.signature_capture, name='sig_capture'),
+    path('<username>/signature/<doc_type>/<signature_file>', views.signature_render, name='sig_render'),
 ]
 
 # Copyright 2019 Office Nomads LLC (http://www.officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
