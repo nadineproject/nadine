8bdbc6f67c695c99eb430ea9a9a78229182c045a
Jacob Sayles <jsayles@gmail.com>
2020-01-17 15:26:26 -0800

    bypassing old models

--- doors/keymaster/tests/tests.py
+++ doors/keymaster/tests/tests.py
@@ -85,15 +85,15 @@ class GatekeeperTestCase(TestCase):
         except Exception:
             self.fail("raised Exception unexpectedly!")
 
-    # def test_encode_decode(self):
-    #     good_config = self.get_config()
-    #     gatekeeper = Gatekeeper(good_config)
-    #     o = "this is a test"
-    #     e = gatekeeper.encode_door_code(o)
-    #     self.assertFalse(o == e)
-    #     d = gatekeeper.decode_door_code(e)
-    #     self.assertFalse(d == e)
-    #     self.assertTrue(d == o)
+        # def test_encode_decode(self):
+        #     good_config = self.get_config()
+        #     gatekeeper = Gatekeeper(good_config)
+        #     o = "this is a test"
+        #     e = gatekeeper.encode_door_code(o)
+        #     self.assertFalse(o == e)
+        #     d = gatekeeper.decode_door_code(e)
+        #     self.assertFalse(d == e)
+        #     self.assertTrue(d == o)
 
 
 # Copyright 2020 Office Nomads LLC (https://officenomads.com/) Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at https://opensource.org/licenses/Apache-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
