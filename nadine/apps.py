9f6e61541194a124b2c75fabea816df29afe3c41
Jacob Sayles <jsayles@gmail.com>
2021-05-20 07:09:12 -0400

    minor updates

--- nadine/apps.py
+++ nadine/apps.py
@@ -3,7 +3,6 @@ from django.apps import AppConfig
 
 class NadineConfig(AppConfig):
     name = 'nadine'
-    default_auto_field = AppConfig.default_auto_field
 
     def ready(self):
         # Load and connect signal recievers
