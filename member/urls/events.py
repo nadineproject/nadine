f9207ffb758f9463d7da670486bca54984530d85
Jacob Sayles <jsayles@gmail.com>
2021-05-21 12:47:58 -0400

    new member video page

--- member/urls/events.py
+++ member/urls/events.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('booking/create/<username>/', events.create_booking, name='create_booking'),
     path('booking/confirm/<room>/<start>/<end>/<date>/<rate>/', events.confirm_booking, name='confirm_booking'),
     path('calendar/', events.calendar, name='calendar'),
+    path('videos/', events.member_videos, name='videos'),
 ]
 
 # Copyright 2021 Office Nomads LLC (https://officenomads.com/) Licensed under the AGPL License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at https://www.gnu.org/licenses/agpl-3.0.html. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-
--- member/views/events.py
+++ member/views/events.py
@@ -299,5 +299,11 @@ def calendar(request):
     return render(request, template, context)
 
 
-# Copyright 2021 Office Nomads LLC (https://officenomads.com/) Licensed under the AGPL License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at https://www.gnu.org/licenses/agpl-3.0.html. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+@login_required
+@user_passes_test(is_active_member, login_url='member:not_active')
+def member_videos(request):
+    context = {}
+    return render(request, 'member/events/member_videos.html', context)
 
+
+# Copyright 2021 Office Nomads LLC (https://officenomads.com/) Licensed under the AGPL License, Version 3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at https://www.gnu.org/licenses/agpl-3.0.html. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
